7c272605d224702bf7a49ba66890f7c9033d3350
nstonic <118763806+nstonic@users.noreply.github.com>
2023-04-01 20:46:00 +0400

    Rewrite data validation logic with DRF

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,10 +1,8 @@
-import phonenumbers
-from django.core.exceptions import ObjectDoesNotExist, ValidationError
-from rest_framework import status
 from rest_framework.response import Response
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework.decorators import api_view
+from rest_framework.serializers import ModelSerializer
 
 from .models import Product, Order, ProductInCart
 
@@ -61,81 +59,34 @@ def product_list_api(request):
     })
 
 
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = ProductInCart
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(ModelSerializer):
+    products = ProductSerializer(many=True, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ['products', 'firstname', 'lastname', 'phonenumber', 'address']
+
+
 @api_view(['POST'])
 def register_order(request):
-    order_obj = request.data
-    if error_message := validate_order(order_obj):
-        return entity_error(error_message)
+    serializer = OrderSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
 
-    products = order_obj.pop('products')
-    order = Order.objects.create(**order_obj)
-    products_in_cart = []
-    for product in products:
-        quantity = product['quantity']
-        try:
-            product = Product.objects.get(pk=product['product'])
-        except ObjectDoesNotExist:
-            return entity_error(f'unknown product with id {product["product"]}')
-        else:
-            products_in_cart.append(
-                ProductInCart(
-                    product=product,
-                    order=order,
-                    quantity=quantity
-                )
-            )
+    products = serializer.validated_data.pop('products')
+    order = Order.objects.create(**serializer.validated_data)
+    products_in_cart = [
+        ProductInCart(
+            product=Product.objects.get(pk=product['product']),
+            order=order,
+            quantity=product['quantity']
+        )
+        for product in products
+    ]
     ProductInCart.objects.bulk_create(products_in_cart)
     return Response({})
-
-
-def entity_error(msg: str):
-    return Response(
-        {'error': msg},
-        status=status.HTTP_422_UNPROCESSABLE_ENTITY
-    )
-
-
-def validate_order(order_obj: dict) -> str | None:
-    order_fields = {'products': list, 'firstname': str, 'lastname': str, 'phonenumber': str, 'address': str}
-    product_fields = {'product': int, 'quantity': int}
-    missing_fields = []
-    null_fields = []
-
-    def validate_fields(obj, required_fields):
-        nonlocal null_fields
-        nonlocal missing_fields
-
-        for field in required_fields:
-            if field not in obj:
-                missing_fields.append(field)
-
-        for field, value in obj.items():
-            if field in required_fields:
-                if not value:
-                    null_fields.append(field)
-                    continue
-                required_type = required_fields[field]
-                if not isinstance(value, required_type):
-                    return f'{field} must be {required_type.__name__}, not {type(value).__name__}'
-
-    if error_message := validate_fields(order_obj, order_fields):
-        return error_message
-
-    if 'products' in order_obj:
-        products = order_obj['products']
-        if not products:
-            return f'products cannot be empty list or null'
-
-        for product in products:
-            if error_message := validate_fields(product, product_fields):
-                return error_message
-
-    if missing_fields:
-        return f'{", ".join(missing_fields)} is required fields'
-
-    if null_fields:
-        return f'{", ".join(null_fields)} cannot be null or empty'
-
-    phone_number = phonenumbers.parse(order_obj['phonenumber'], 'RU')
-    if not phonenumbers.is_valid_number(phone_number):
-        return 'invalid phonenumber'
