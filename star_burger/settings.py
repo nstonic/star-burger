05d638144e7a85bbb18cc5bb5a735d12fa75e91b
nstonic <nstonic@gmail.com>
2023-04-17 14:13:30 +0400

    Add delimiter

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 GEOCODER_API_KEY = env('GEOCODER_API_KEY')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'], delimiter=',')
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
